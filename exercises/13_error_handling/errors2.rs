893e35c857546fa4edaad97f87b81c2a87865088
pedroco3lho <pedrocoelho.nascimento@gmail.com>
2025-06-06 09:55:12 -0300

    fix: error handling

--- exercises/13_error_handling/errors2.rs
+++ exercises/13_error_handling/errors2.rs
@@ -14,7 +14,7 @@
 // There are at least two ways to implement this that are both correct. But one
 // is a lot shorter!
 
-use std::num::ParseIntError;
+use std::{f32::consts::E, num::ParseIntError};
 
 fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
     let processing_fee = 1;
@@ -23,7 +23,11 @@ fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
     // TODO: Handle the error case as described above.
     let qty = item_quantity.parse::<i32>();
 
-    Ok(qty * cost_per_item + processing_fee)
+    match qty {
+        Err(e) => Err(e),
+        Ok(n) => Ok(n * cost_per_item + processing_fee)
+    }
+
 }
 
 fn main() {
